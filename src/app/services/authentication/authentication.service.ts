3b775f7bf357cbfda86ef5b9a40d34b4f0903587
Prateek Kher <prateek.kher@gmail.com>
2023-09-13 09:40:43 +0530

    Updated Authentication Service and added Environment

--- src/app/services/authentication/authentication.service.ts
+++ src/app/services/authentication/authentication.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
+import { User } from './user.model';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private _user: User | null;
+
+  constructor(private api: ApiService) { }
+
+  isLoggedIn(): boolean {
+    if (this._user === null) {
+      return false;
+    }
+
+    if (this._user.token === null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  authenticate(username: string, password: string) {
+    this.api.fetch_data(['user', 'authenticate'], {username, password})
+  }
   
 }
