df797fadda9ef08acfca3b1205ec90aee3645067
Prateek Kher <prateek.kher@gmail.com>
2023-10-01 15:23:35 +0530

    Added master routes

--- src/app/modules/secured/nav-bar/nav-bar.component.ts
+++ src/app/modules/secured/nav-bar/nav-bar.component.ts
@@ -4,7 +4,7 @@ import { navItems } from './menu-items.data';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
   items = navItems;
