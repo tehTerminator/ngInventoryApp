c8724fdef8dc67d240c008e265042219821eac05
Prateek Kher <prateek.kher@gmail.com>
2023-10-10 12:06:01 +0530

    Added Skeleton Statement form and Table

--- src/app/modules/secured/vouchers/statement/statement.module.ts
+++ src/app/modules/secured/vouchers/statement/statement.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { StatementRoutingModule } from './statement-routing.module';
 import { StatementComponent } from './statement.component';
+import { FormComponent } from './components/form/form.component';
+import { TableComponent } from './components/table/table.component';
+
+import { CoreModule } from './../../../core/core.module';
 
 
 @NgModule({
   declarations: [
-    StatementComponent
+    StatementComponent,
+    FormComponent,
+    TableComponent
   ],
   imports: [
     CommonModule,
-    StatementRoutingModule
+    StatementRoutingModule,
+    CoreModule
   ]
 })
 export class StatementModule { }
