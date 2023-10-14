98ce738e724735e44c5c725b18118bc002108278
Prateek Kher <prateek.kher@gmail.com>
2023-10-14 16:57:02 +0530

    change kind from uppercase to lowercase

--- src/app/interface/invoice.ts
+++ src/app/interface/invoice.ts
@@ -1,12 +1,13 @@
 import { Entity } from './entity.interface';
+import { Transaction } from './transaction';
 
 export interface Invoice extends Entity {
     id: number;
-    kind: 'SALES' | 'PURCHASE';
+    kind: 'sales' | 'purchase';
     contactId: number;
     locationId: number;
     paid: boolean;
     amount: number;
     userId: number;
-    transactions: InvoiceTransaction[];
+    transactions: Transaction[];
 }
