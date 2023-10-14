086015fbcacd82d7b34894cd026173f29cf2505a
Prateek Kher <prateek.kher@gmail.com>
2023-10-14 15:13:03 +0530

    Function but not production Ready

--- src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
+++ src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { InvoiceStoreService } from '../../../services/invoice-store.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-select-product',
   templateUrl: './select-product.component.html',
-  styleUrls: ['./select-product.component.scss']
+  styleUrls: ['./select-product.component.scss'],
 })
 export class SelectProductComponent {
+  productControl = new FormControl();
+  products = [
+    {
+      id: 1,
+      title: 'Product 1',
+    },
+    {
+      id: 2,
+      title: 'Product 2',
+    },
+    {
+      id: 3,
+      title: 'Product 3',
+    },
+  ]; // Replace with your product data
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  onSelectProduct() {
+    // Get the selected product from the form control
+    const selectedProduct = this.productControl.value;
+
+    if (selectedProduct) {
+      // Set the selected product in the InvoiceService
+
+      this.navigateToCreateTransactions(selectedProduct);
+    }
+  }
+
+  navigateToCreateTransactions(product_id: number) {
+    console.log(product_id)
+    // Get the current :type parameter from the route
+    const type = this.route.snapshot.paramMap.get('type');
+
+    // Navigate to the relative path for select-product
+    this.router.navigate(['../create-transactions'], {
+      relativeTo: this.route,
+      queryParams: { type, product_id },
+    });
+  }
 }
