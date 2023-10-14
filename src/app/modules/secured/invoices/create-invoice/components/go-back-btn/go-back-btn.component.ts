60352436ea1427c2049a9a22c33d3aac937ec3e6
Prateek Kher <prateek.kher@gmail.com>
2023-10-14 15:12:46 +0530

    Changed functionality from ShopApp3 to InventoryManagement

--- src/app/modules/secured/invoices/create-invoice/components/go-back-btn/go-back-btn.component.ts
+++ src/app/modules/secured/invoices/create-invoice/components/go-back-btn/go-back-btn.component.ts
@@ -4,10 +4,10 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-go-back-btn',
   templateUrl: './go-back-btn.component.html',
-  styleUrls: ['./go-back-btn.component.css']
+  styleUrls: ['./go-back-btn.component.css'],
 })
 export class GoBackBtnComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   goBack(): void {
     const path = this.generateRoute();
@@ -15,29 +15,23 @@ export class GoBackBtnComponent {
   }
 
   get currentPath(): string[] {
-    const url = this.router.url;
-    return url.slice(1, url.length).split('/');
+    return this.router.url.split('/').filter(segment => !!segment);
   }
 
   private generateRoute(): string[] {
-    const path = ['/invoices', 'create'];
+    const path = this.currentPath.slice();
 
-    if (this.currentPath.length < 3 && this.currentPath[1] !== 'create') {
+    if (path.length < 5 || path[2] !== 'create') {
       return [];
     }
 
-    switch (this.currentPath[2]) {
-      case 'list-items':
-        path.push('select-customer');
-        break;
-      case 'transactions':
-        path.push('list-items');
-        break;
-      case 'paymentMethod':
-        path.push('list-items');
+    switch (path[4]) {
+      case 'create-transactions':
+      case 'payment-method':
+        path[4] = 'select-product';
         break;
       default:
-        path.push('select-customer');
+        path[4] = 'select-customer';
         break;
     }
 
@@ -45,18 +39,9 @@ export class GoBackBtnComponent {
   }
 
   showBtn(): boolean {
-    if (this.currentPath.length < 3) {
+    if (this.currentPath.length < 3 || this.currentPath[2] !== 'create' || this.currentPath[4] === 'select-customer') {
       return false;
     }
-
-    if (this.currentPath[1] !== 'create') {
-      return false;
-    }
-
-    if (this.currentPath[2] === 'select-customer'){
-      return false;
-    }
-
     return true;
   }
 }
