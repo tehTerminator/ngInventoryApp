906a56ab0a9a1aeece46cbfd44289b0b7eae020c
Prateek Kher <prateek.kher@gmail.com>
2023-10-20 18:21:38 +0530

    Updated Definition

--- src/app/interface/balance-snapshot.ts
+++ src/app/interface/balance-snapshot.ts
@@ -1,2 +1,9 @@
-export interface BalanceSnapshot {
+import { Entity } from "./entity.interface";
+import { Ledger } from './ledger';
+
+export interface BalanceSnapshot extends Entity{
+    ledger_id: number;
+    ledger: Ledger;
+    opening: number;
+    closing: number;
 }
