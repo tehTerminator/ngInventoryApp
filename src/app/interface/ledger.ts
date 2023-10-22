c043ba186f112dd1c4c04f5846e199eb8b8884dd
Prateek Kher <prateek.kher@gmail.com>
2023-10-22 16:59:02 +0530

    Added Empty Ledger

--- src/app/interface/ledger.ts
+++ src/app/interface/ledger.ts
@@ -6,3 +6,12 @@ export interface Ledger extends Entity {
   canReceivePayment: boolean;
   kind: 'BANK' | 'WALLET' | 'DEPOSITS' | 'CASH' | 'PAYABLE' | 'RECEIVABLE' | 'EXPENSES' | 'INCOME';
 }
+
+export const EMPTYLEDGER: Ledger = Object.freeze({
+  title: '',
+  groupId: 0,
+  canReceivePayment: false,
+  kind: 'BANK',
+  id: 0
+});
+
