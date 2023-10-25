1e8648c08ae993aea78f07eaa75fc99c59ce7b7e
Prateek Kher <prateek.kher@gmail.com>
2023-10-25 14:14:21 +0530

    Created MyLocationService

--- src/app/services/myLocation/my-location.service.ts
+++ src/app/services/myLocation/my-location.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { EMPTYLOCATION, StoreLocation } from './../../interface/location';
+import { ApiService } from './../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyLocationService {
+  private _myLocations = new BehaviorSubject<StoreLocation[]>([]);
+  private _selectedLocation = new BehaviorSubject<StoreLocation>(EMPTYLOCATION);
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  retrieveData() {
+    this.api.retrieve<StoreLocation[]>(['get', 'user', 'locations'])
+    .subscribe({
+      next: (value) => {
+        this._myLocations.next(value);
+        if(value.length === 1) {
+          this._selectedLocation.next(value[0]);
+        }
+      }
+    });
+  }
+
+  set selectedLocation(value: StoreLocation) {
+    this._selectedLocation.next(value);
+  }
+
+  get myLocations(): Observable<StoreLocation[]> {
+    return this._myLocations;
+  }
+
+  get selectedLocation(): Observable<StoreLocation> {
+    return this._selectedLocation;
+  }
 }
