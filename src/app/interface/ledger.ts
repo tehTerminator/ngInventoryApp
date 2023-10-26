a21f31ea2884ac3690d981f869c3eb87f1ad81bd
Prateek Kher <prateek.kher@gmail.com>
2023-10-26 16:32:04 +0530

    Added More Types

--- src/app/interface/ledger.ts
+++ src/app/interface/ledger.ts
@@ -1,10 +1,22 @@
-import { Entity } from "./entity.interface";
+import { Entity } from './entity.interface';
 
 export interface Ledger extends Entity {
   title: string;
   groupId: number;
   canReceivePayment: boolean;
-  kind: 'BANK' | 'WALLET' | 'DEPOSITS' | 'CASH' | 'PAYABLE' | 'RECEIVABLE' | 'EXPENSES' | 'INCOME';
+  kind:
+    | 'CAPITAL'
+    | 'BANK'
+    | 'WALLET'
+    | 'DEPOSIT'
+    | 'CASH'
+    | 'PAYABLE'
+    | 'RECEIVABLE'
+    | 'EXPENSE'
+    | 'INCOME'
+    | 'PURCHASE AC'
+    | 'SALES AC'
+    | 'DUTIES AND TAXES';
 }
 
 export const EMPTYLEDGER: Ledger = Object.freeze({
@@ -12,6 +24,5 @@ export const EMPTYLEDGER: Ledger = Object.freeze({
   groupId: 0,
   canReceivePayment: false,
   kind: 'BANK',
-  id: 0
+  id: 0,
 });
-
