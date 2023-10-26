87e88667171880b58f6062d2cf6a2402899f3373
Prateek Kher <prateek.kher@gmail.com>
2023-10-26 22:01:36 +0530

    Added view Path

--- src/app/modules/secured/invoices/invoices-routing.module.ts
+++ src/app/modules/secured/invoices/invoices-routing.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, TitleStrategy } from '@angular/router';
 import { InvoicesComponent } from './invoices.component';
 import { TemplatePageTitleStrategy } from './../../../services/title-strategy/title-strategy';
+import { PreviewInvoiceComponent } from './shared-components/preview-invoice/preview-invoice.component';
 
 const routes: Routes = [
   { path: '', component: InvoicesComponent },
-  { path: 'create/:type', loadChildren: () => import('./create-invoice/create-invoice.module').then(m => m.CreateInvoiceModule) }
+  {
+    path: 'create/:type',
+    loadChildren: () =>
+      import('./create-invoice/create-invoice.module').then(
+        (m) => m.CreateInvoiceModule
+      ),
+  },
+  {
+    path: 'view/:id',
+    component: PreviewInvoiceComponent
+  }
 ];
 
 // const routes: Routes = [
@@ -31,9 +42,11 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [{
-    provide: TitleStrategy,
-    useClass: TemplatePageTitleStrategy
-  }]
+  providers: [
+    {
+      provide: TitleStrategy,
+      useClass: TemplatePageTitleStrategy,
+    },
+  ],
 })
-export class InvoicesRoutingModule { }
+export class InvoicesRoutingModule {}
