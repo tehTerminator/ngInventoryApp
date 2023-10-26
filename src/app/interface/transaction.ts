677f8dcbaf848b1911aa36ffebf2e4bd8d35a24c
Prateek Kher <prateek.kher@gmail.com>
2023-10-26 16:32:47 +0530

    Fixed Product[] which should be Product

--- src/app/interface/transaction.ts
+++ src/app/interface/transaction.ts
@@ -4,7 +4,7 @@ import { Product } from './product';
 export interface Transaction extends Entity {
     invoice_id: number;
     product_id: number;
-    product: Product[]
+    product: Product;
     user_id: number;
     narration: string;
     quantity: number;
