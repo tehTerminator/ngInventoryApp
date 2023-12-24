95c2d3433ad9911dad7b97ad329686f43eea8f5c
Prateek Kher <prateek.kher@gmail.com>
2023-12-24 20:53:33 +0530

    Added Rate removed Product-Groups

--- src/app/interface/product.ts
+++ src/app/interface/product.ts
@@ -1,7 +1,6 @@
 import { Entity } from './entity.interface';
-import { ProductGroup } from './product-group';
 
 export interface Product extends Entity {
     title: string;
-    group: ProductGroup;
-}
+    rate: number;
+}    
