3aef84d6e27ba6ec65cbbd107e9c2ffeb3987ee8
Prateek Kher <prateek.kher@gmail.com>
2023-12-24 20:53:54 +0530

    Removed OnDestroy and Subscription

--- src/app/modules/secured/nav-bar/nav-bar.component.ts
+++ src/app/modules/secured/nav-bar/nav-bar.component.ts
@@ -1,26 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { navItems } from './menu-items.data';
 import { AuthStoreService } from './../../../services/auth-store/auth-store.service';
-import { Observable, Subscription, map } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit, OnDestroy {
+export class NavBarComponent implements OnInit {
   items = navItems;
-  private _sub = new Subscription();
   private _role = 'admin';
   constructor(private userStore: AuthStoreService) {}
 
   ngOnInit(): void {
     this._role = this.userStore.userData.role;
-  }
-
-  ngOnDestroy(): void {
-    // this._sub.unsubscribe();
-    console.log('Destroyed');
   }
 
   hasRole(role: string | undefined): boolean {
