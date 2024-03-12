5696f50efd0d986b2f1d1f98e30aa5ff0c7d9429
Prateek Kher <prateek.kher@gmail.com>
2024-03-12 08:43:53 +0530

    Added Menu Data

---
+++ src/app/modules/secure-module/nav-bar/menu-items.data.ts
@@ -0,0 +1,58 @@
+export const navItems = [
+    {
+      title: 'Accounting',
+      links: [
+        { text: 'Ledger', routerLink: ['accounting', 'ledgers'] },
+        { text: 'Vouchers', routerLink: ['accounting', 'voucher'] },
+        { text: 'Statement', routerLink: ['accounting', 'statement'] },
+        { text: 'Day Book', routerLink: ['accounting', 'daybook'] },
+      ],
+    },
+    {
+      title: 'Invoices',
+      links: [
+        { text: 'Sales Invoices', routerLink: ['invoices', 'create', 'sales'] },
+        {
+          text: 'Purchase Invoices',
+          routerLink: ['invoices', 'create', 'purchase'],
+        },
+        { text: 'View Invoices', routerLink: ['invoices', 'search'] },
+      ],
+    },
+    {
+      title: 'Products',
+      links: [
+        { text: 'New Product', routerLink: ['products', 'add'] },
+        { text: 'View Product', routerLink: ['products', 'view'] },
+        { text: 'Transfer Product', routerLink: ['products', 'transfer'] },
+      ],
+    },
+    {
+      title: 'Product-Groups',
+      links: [
+        { text: 'New Group', routerLink: ['products', 'product-group', 'add'] },
+        {
+          text: 'View Groups',
+          routerLink: ['products', 'product-group', 'view'],
+        },
+      ],
+    },
+    {
+      title: 'Locations',
+      links: [
+        { text: 'New Store', routerLink: ['locations', 'add'] },
+        { text: 'View Locations', routerLink: ['locations', 'view'] },
+        {
+          text: 'My Location',
+          routerLink: ['locations', 'select-location'],
+        },
+      ],
+    },
+    {
+      title: 'Users',
+      links: [
+        { text: 'Create New', routerLink: ['master', 'users', 'create-user'] },
+      ],
+      role: 'admin'
+    },
+  ];
--- src/app/modules/secure-module/nav-bar/nav-bar.component.ts
+++ src/app/modules/secure-module/nav-bar/nav-bar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthStoreService } from '../../../services/auth-store/auth-store.service';
+import { navItems } from './menu-items.data';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
+  items = navItems;
+  // private _sub = new Subscription();
+  // private _role = 'admin';
+  constructor(private userStore: AuthStoreService) {}
 
+  ngOnInit(): void {
+    // this._role = this.userStore.userData.role;
+  }
+
+  ngOnDestroy(): void {
+    // this._sub.unsubscribe();
+    console.log('Destroyed');
+  }
+
+  // hasRole(role: string | undefined): boolean {
+  //   if (role === undefined) {
+  //     return true;
+  //   }
+  //   return role === this._role;
+  // }
 }
