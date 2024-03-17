4ae8ba27b5a42bb96ac7df9707a4fd802e555b38
Prateek Kher <prateek.kher@gmail.com>
2024-03-17 23:13:07 +0530

    Added ContactService

--- src/app/modules/secured/invoices/invoices.module.ts
+++ src/app/modules/secured/invoices/invoices.module.ts
@@ -6,7 +6,7 @@ import { InvoicesRoutingModule } from './invoices-routing.module';
 import { InvoicesComponent } from './invoices.component';
 import { SharedComponentModule } from './shared-components/shared-components.module';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { CustomerService } from './services/customer.service';
+import { ContactsService } from './services/contacts.service';
 import { InvoiceStoreService } from './services/invoice-store.service';
 import { PleaseWaitPageComponent } from './please-wait-page/please-wait-page.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
@@ -27,7 +27,8 @@ import { SearchInvoiceStoreService } from './search-invoice/search-store/search-
   ],
   providers: [
     InvoiceStoreService,
-    SearchInvoiceStoreService
+    SearchInvoiceStoreService,
+    ContactsService
   ]
 })
 export class InvoicesModule { }
