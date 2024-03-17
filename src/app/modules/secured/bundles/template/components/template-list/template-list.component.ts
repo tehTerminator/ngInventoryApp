1a2a1d6bdbb895af5f9692a68a25577f18b66d8f
Prateek Kher <prateek.kher@gmail.com>
2024-03-17 16:29:15 +0530

    Created List

--- src/app/modules/secured/bundles/template/components/template-list/template-list.component.ts
+++ src/app/modules/secured/bundles/template/components/template-list/template-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BundleStoreService } from './../../services/bundle-store.service';
+import { Observable } from 'rxjs';
+import { BundleTemplate } from '../../../../../../interface/bundles';
 
 @Component({
   selector: 'app-template-list',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './template-list.component.scss'
 })
 export class TemplateListComponent {
+  constructor(private store: BundleStoreService) {}
 
+  get templates(): Observable<BundleTemplate[]> {
+    return this.store.templates$;
+  }
+
+  get totalAmount(): number {
+    return this.store.totalAmount;
+  }
 }
