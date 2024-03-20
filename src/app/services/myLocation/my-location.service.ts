2174245a02d98fb73873d4763242bf53dc917874
Prateek Kher <prateek.kher@gmail.com>
2024-03-20 08:31:51 +0530

    added .interface to files containing Interface

--- src/app/services/myLocation/my-location.service.ts
+++ src/app/services/myLocation/my-location.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { EMPTYLOCATION, StoreLocation } from './../../interface/location';
+import { EMPTYLOCATION, StoreLocation } from '../../interface/location.interface';
 import { ApiService } from './../api/api.service';
 
 @Injectable({
