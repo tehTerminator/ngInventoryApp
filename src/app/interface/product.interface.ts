cb5f07f6b1de14ea400d750eac1d7df35df2df5c
Prateek Kher <prateek.kher@gmail.com>
2024-03-20 08:31:00 +0530

    Added EMPTY_PRODUCT

--- src/app/interface/product.interface.ts
+++ src/app/interface/product.interface.ts
@@ -4,3 +4,9 @@ export interface Product extends Entity {
     title: string;
     rate: number;
 }    
+
+export const EMPTY_PRODUCT: Product = Object.freeze({
+    id: 0,
+    title: 'EMPTY_PRODUCT',
+    rate: 0,
+});
