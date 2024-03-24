3067b0d820ca49bbcd3aeae33e379d3ddee32b92
Prateek Kher <prateek.kher@gmail.com>
2024-03-24 23:19:58 +0530

    now uses service service to fetch title

--- src/app/modules/secured/invoices/invoice-list/invoice-list.component.ts
+++ src/app/modules/secured/invoices/invoice-list/invoice-list.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { SearchInvoiceStoreService } from '../search-invoice/search-store/search-store.service';
 import { Invoice } from '../../../../interface/invoice.interface';
 import { Observable } from 'rxjs';
+import { ContactsService } from '../services/contacts.service';
+import { EMPTY_CONTACT } from '../../../../interface/contact.interface';
 
 @Component({
   selector: 'app-invoice-list',
@@ -9,9 +11,21 @@ import { Observable } from 'rxjs';
   styleUrls: ['./invoice-list.component.scss']
 })
 export class InvoiceListComponent {
-  constructor(private store: SearchInvoiceStoreService) {}
+  constructor(
+    private store: SearchInvoiceStoreService,
+    private contactService: ContactsService,
+  ) {}
 
   get invoices(): Observable<Invoice[]> {
     return this.store.invoices;
   }
+
+  contact(id: number) {
+    try {
+      return this.contactService.getElementById(id);
+    } catch (e) {
+      return EMPTY_CONTACT;
+    }
+  }
+
 }
