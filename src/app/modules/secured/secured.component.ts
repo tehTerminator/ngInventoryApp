d9617b982c6a8ce2f14331ea38633b9da5aac1b6
Prateek Kher <prateek.kher@gmail.com>
2024-03-24 23:15:05 +0530

    initializing various services on load

--- src/app/modules/secured/secured.component.ts
+++ src/app/modules/secured/secured.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthenticationService } from './../../services/authentication/authentication.service';
+import { UserService } from '../../services/user/user.service';
+import { LocationService } from '../../services/locations/locations.service';
 
 @Component({
   selector: 'app-secured',
   templateUrl: './secured.component.html',
   styleUrls: ['./secured.component.scss']
 })
-export class SecuredComponent {
-  constructor(private authService: AuthenticationService) {}
+export class SecuredComponent implements OnInit {
+  constructor(
+    private authService: AuthenticationService,
+    private userService: UserService,
+    private locationService: LocationService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.init();
+    this.locationService.init();
+  }
 
   logout = () => this.authService.signOut();
 }
