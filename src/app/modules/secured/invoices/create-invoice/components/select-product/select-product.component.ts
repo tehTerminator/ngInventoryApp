9beb49c8eff65834d8a82b5c9139e07f0294ffbe
Prateek Kher <prateek.kher@gmail.com>
2024-03-24 23:18:55 +0530

    now uses Service for Populating Results

--- src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
+++ src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
@@ -1,67 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { InvoiceStoreService } from '../../../services/invoice-store.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { ApiService } from './../../../../../../services/api/api.service';
-import { MyLocationStoreService } from './../../../../../../services/myLocation/my-location.service';
-import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { Observable, Subscription, map } from 'rxjs';
+import { GeneralItem } from '../../../../../../interface/general-item.interface';
+import { GeneralItemStoreService } from '../../services/general-item-store.service';
 import { Product } from '../../../../../../interface/product.interface';
-import { StockInfo } from '../../../../../../interface/stock-info.interface';
-import { StoreLocation } from '../../../../../../interface/location.interface';
+import { ProductService } from '../../../../../../services/product/product.service';
 
 @Component({
   selector: 'app-select-product',
   templateUrl: './select-product.component.html',
   styleUrls: ['./select-product.component.scss'],
 })
-export class SelectProductComponent implements OnInit {
-  productControl = new FormControl();
-  private _products = new BehaviorSubject<StockInfo[]>([]);
+export class SelectProductComponent implements OnInit, OnDestroy {
+  productControl = new FormControl<GeneralItem | null>(null);
+  private type = 'SALES';
+  private _sub = new Subscription();
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private api: ApiService,
-    private myLocationStore: MyLocationStoreService,
-    private store: InvoiceStoreService
+    private store: InvoiceStoreService,
+    private generalItemStore: GeneralItemStoreService,
+    private productService: ProductService
   ) {}
 
   ngOnInit(): void {
-    const storeLocation = this.myLocationStore.snapshot.selected;
-    this.loadProducts(storeLocation);
+    this.generalItemStore.init();
+    this.productService.init();
+  }
+
+  ngOnDestroy(): void {
+    this._sub.unsubscribe;
   }
 
   onSelectProduct() {
     // Get the selected product from the form control
-    const selectedProduct: Product = this.productControl.value;
+    const selectedProduct: GeneralItem | null = this.productControl.value;
 
     if (!!selectedProduct) {
-      this.store.selectedItem = selectedProduct;
+      this.store.selectedItem = this.generalItemStore.selectActualItem(selectedProduct);
       this.navigateToCreateTransactions();
     }
   }
 
-  loadProducts(value: StoreLocation): void {
-    console.log('Load Product Called');
-    this.api
-      .retrieve<StockInfo[]>(['location', 'inventory'], {
-        id: value.id.toString(),
-      })
-      .subscribe({
-        next: (value) => this._products.next(value),
-      });
-  }
-
-  navigateToCreateTransactions() {
-    // Get the current :type parameter from the route
-
-    // Navigate to the relative path for select-product
+  private navigateToCreateTransactions() {
     this.router.navigate(['../create-transactions'], {
       relativeTo: this.route,
     });
   }
 
-  get products$(): Observable<StockInfo[]> {
-    return this._products;
+  get products$(): Observable<GeneralItem[] | Product[]> {
+    if ( this.store.kind === 'SALES' ) {
+      return this.generalItemStore.getAsObservable();
+    } 
+    return this.productService.getAsObservable();
   }
 
   get hasTransactions(): Observable<boolean> {
