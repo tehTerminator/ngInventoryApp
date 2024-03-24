ecc7acd3c2dd036a712870a6f419189fa2e2a2fc
Prateek Kher <prateek.kher@gmail.com>
2024-03-24 23:15:32 +0530

    initializing services

--- src/app/modules/secured/invoices/invoices.component.ts
+++ src/app/modules/secured/invoices/invoices.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from './services/contacts.service';
+import { LedgerService } from '../../../services/ledger/ledger.service';
+import { ProductService } from '../../../services/product/product.service';
+import { BundleService } from '../../../services/bundle/bundle.service';
 // import { LedgerService } from '../../shared/services/ledger/ledger.service';
 // import { PosItemService } from '../../shared/services/posItem/pos-item.service';
 // import { ProductService } from '../../shared/services/product/product.service';
@@ -15,16 +19,18 @@ export class InvoicesComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    // private ledgerService: LedgerService,
-    // private productService: ProductService,
-    // private posItemService: PosItemService,
+    private contactService: ContactsService,
+    private ledgerService: LedgerService,
+    private productService: ProductService,
+    private bundleService: BundleService
   ) { }
 
   ngOnInit(): void {
     // Initialize Services to Be Used Later
-    // this.ledgerService.init();
-    // this.productService.init();
-    // this.posItemService.init();
+    this.ledgerService.init();
+    this.productService.init();
+    this.bundleService.init();
+    this.contactService.init();
   }
 
 
