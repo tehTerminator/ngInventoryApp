532c88eb092c4f08c8fc4df02f61e8d3fe233c8e
Prateek Kher <prateek.kher@gmail.com>
2024-03-24 23:19:28 +0530

    now extends BaseService

--- src/app/modules/secured/invoices/create-invoice/services/general-item-store.service.ts
+++ src/app/modules/secured/invoices/create-invoice/services/general-item-store.service.ts
@@ -3,51 +3,66 @@ import { LedgerService } from './../../../../../services/ledger/ledger.service';
 import { ProductService } from './../../../../../services/product/product.service';
 import { BundleService } from './../../../../../services/bundle/bundle.service';
 import { ApiService } from '../../../../../services/api/api.service';
-import { GeneralItem } from './../../../../../interface/general-item';
+import { GeneralItem } from './../../../../../interface/general-item.interface';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Bundle } from '../../../../../interface/bundle.interface';
 import { Ledger } from '../../../../../interface/ledger.interface';
 import { Product } from '../../../../../interface/product.interface';
+import { MyLocationStoreService } from '../../../../../services/myLocation/my-location.service';
+import { BaseService } from '../../../../../class/BaseService';
+import { HOUR } from '../../../../../shared/constants';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GeneralItemStoreService {
-  private data: GeneralItem[] = [];
+export class GeneralItemStoreService extends BaseService<GeneralItem> {
+  protected fetch(): void {
+    const mylocationId = this.myLocationService.snapshot.selected.id;
+
+    this.apiService
+      .retrieve<GeneralItem[]>('general-items', {
+        locationId: mylocationId.toString(),
+      })
+      .subscribe({
+        next: (res) => (this.store(res)),
+        error: (err) => {
+          this._data.next([]);
+          console.error(err);
+        },
+      });
+  }
+  public create(data: GeneralItem): Observable<GeneralItem> {
+    throw new Error('Method not implemented.');
+  }
+  public update(data: GeneralItem): Observable<GeneralItem> {
+    throw new Error('Method not implemented.');
+  }
+  public delete(id: number): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
 
   constructor(
     private ledgerService: LedgerService,
     private productService: ProductService,
     private bundleService: BundleService,
-    private apiService: ApiService
-  ) {}
-
-  init(): void {
+    private apiService: ApiService,
+    private myLocationService: MyLocationStoreService
+  ) {
+    super('general-items', HOUR);
     this.ledgerService.init();
     this.productService.init();
     this.bundleService.init();
-
-    this.apiService.retrieve<GeneralItem[]>('general-items').subscribe({
-      next: (res) => (this.data = res),
-      error: (err) => {
-        console.log(err);
-        this.data = [];
-      },
-    });
   }
 
-  get items(): GeneralItem[] {
-    return this.data;
-  }
 
   selectActualItem(item: GeneralItem): Product | Ledger | Bundle {
     switch (item.type) {
       case 'PRODUCT':
-        return this.productService.getElementById(item.id) as Product;
+        return this.productService.getElementById(item.id);
       case 'LEDGER':
-        return this.ledgerService.getElementById(item.id) as Ledger;
+        return this.ledgerService.getElementById(item.id);
       default:
-        return this.bundleService.getElementById(item.id) as Bundle;
+        return this.bundleService.getElementById(item.id);
     }
   }
 }
