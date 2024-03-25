cfe7e47e71b6c072a8c670dfe5a87f9fef744e22
Prateek Kher <prateek.kher@gmail.com>
2024-03-25 23:51:32 +0530

    Removed InvoiceStoreService from Providers

--- src/app/modules/secured/invoices/invoices.module.ts
+++ src/app/modules/secured/invoices/invoices.module.ts
@@ -6,11 +6,9 @@ import { InvoicesRoutingModule } from './invoices-routing.module';
 import { InvoicesComponent } from './invoices.component';
 import { SharedComponentModule } from './shared-components/shared-components.module';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { ContactsService } from './services/contacts.service';
 import { PleaseWaitPageComponent } from './please-wait-page/please-wait-page.component';
 import { InvoiceListComponent } from './invoice-list/invoice-list.component';
 import { SearchInvoiceStoreService } from './search-invoice/search-store/search-store.service';
-import { InvoiceStoreService } from './services/invoice-store.service';
 
 @NgModule({
   declarations: [
@@ -26,9 +24,7 @@ import { InvoiceStoreService } from './services/invoice-store.service';
     MatAutocompleteModule
   ],
   providers: [
-    InvoiceStoreService,
     SearchInvoiceStoreService,
-    ContactsService
   ]
 })
 export class InvoicesModule { }
