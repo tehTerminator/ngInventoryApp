163730dafe85ff60d40b35c9411a3ab2cc44bff0
Prateek Kher <prateek.kher@gmail.com>
2024-03-25 14:48:23 +0530

    updated Frequency HOUR

--- src/app/modules/secured/invoices/services/contacts.service.ts
+++ src/app/modules/secured/invoices/services/contacts.service.ts
@@ -4,36 +4,36 @@ import { catchError, tap } from 'rxjs/operators';
 import { BaseService } from '../../../../class/BaseService';
 import { Contact } from '../../../../interface/contact.interface';
 import { ApiService } from '../../../../services/api/api.service';
+import { HOUR } from '../../../../interface/collection.interface';
 
 @Injectable()
 export class ContactsService extends BaseService<Contact> {
+  constructor(private api: ApiService) {
+    super('contacts', HOUR);
+  }
 
-    constructor(private api: ApiService) {
-        super('contacts', 60);
-    }
+  protected fetch(): void {
+    this.api
+      .retrieve<Contact[]>(this.table)
+      .subscribe((customers) => this.store(customers));
+  }
 
-    protected fetch(): void {
-        this.api.retrieve<Contact[]>(this.table)
-        .subscribe(customers => this.store(customers));
-    }
+  public create(customer: Contact): Observable<Contact> {
+    return this.api.create<Contact>('contact', customer).pipe(
+      tap((response) => {
+        this.insert(response);
+      }),
+      catchError((error) => {
+        console.log(error);
+        throw new Error('Unable to Create New Customer');
+      })
+    );
+  }
 
-    public create(customer: Contact): Observable<Contact> {
-        return this.api.create<Contact>([this.table], customer)
-        .pipe(
-            tap(response => {
-                this.insert(response);
-            }),
-            catchError(error => {
-                console.log(error);
-                throw new Error('Unable to Create New Customer');
-            })
-        );
-    }
-
-    public update(customer: Contact): Observable<Contact> {
-        throw new Error('Method not implemented.');
-    }
-    public delete(id: number): Observable<any> {
-        throw new Error('Method not implemented.');
-    }
+  public update(customer: Contact): Observable<Contact> {
+    throw new Error('Method not implemented.');
+  }
+  public delete(id: number): Observable<any> {
+    throw new Error('Method not implemented.');
+  }
 }
