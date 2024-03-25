8c3ab245a75a6a608b215ba33bc39e55c4dd4722
Prateek Kher <prateek.kher@gmail.com>
2024-03-25 15:16:53 +0530

    Added MatAutoComplete

--- src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
+++ src/app/modules/secured/invoices/create-invoice/components/select-product/select-product.component.ts
@@ -2,12 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { InvoiceStoreService } from '../../../services/invoice-store.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormControl } from '@angular/forms';
-import { ApiService } from './../../../../../../services/api/api.service';
-import { Observable, Subscription, map } from 'rxjs';
+import { Observable, Subscription, map, startWith, debounceTime, EMPTY } from 'rxjs';
 import { GeneralItem } from '../../../../../../interface/general-item.interface';
 import { GeneralItemStoreService } from '../../services/general-item-store.service';
 import { Product } from '../../../../../../interface/product.interface';
 import { ProductService } from '../../../../../../services/product/product.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { NotificationsService } from '../../../../../../services/notification/notification.service';
 
 @Component({
   selector: 'app-select-product',
@@ -15,53 +16,83 @@ import { ProductService } from '../../../../../../services/product/product.servi
   styleUrls: ['./select-product.component.scss'],
 })
 export class SelectProductComponent implements OnInit, OnDestroy {
-  productControl = new FormControl<GeneralItem | null>(null);
-  private type = 'SALES';
+  productControl = new FormControl<GeneralItem | Product | ''>('', {nonNullable: true});
+  filteredProducts$: Observable<GeneralItem[] | Product[]> = EMPTY;
   private _sub = new Subscription();
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private store: InvoiceStoreService,
     private generalItemStore: GeneralItemStoreService,
-    private productService: ProductService
+    private productService: ProductService,
+    private notification: NotificationsService,
   ) {}
 
   ngOnInit(): void {
     this.generalItemStore.init();
     this.productService.init();
+
+    // Combine observable streams for product options
+    this.filteredProducts$ = this.productControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(300), // Adjust debounce time as needed (optional)
+      map((value) => {
+        if (typeof(value) === 'string'){
+          return this._filterProducts(value)
+        } 
+        return [];
+      })
+    );
   }
 
   ngOnDestroy(): void {
-    this._sub.unsubscribe;
+    this._sub.unsubscribe();
   }
 
-  onSelectProduct() {
-    // Get the selected product from the form control
-    const selectedProduct: GeneralItem | null = this.productControl.value;
-
-    if (!!selectedProduct) {
+  onSelectProduct(event: MatAutocompleteSelectedEvent) {
+    const selectedProduct: GeneralItem | Product | null = event.option.value;
+    if (selectedProduct === null ) {
+      this.notification.show('Invalid Product Selected');
+    }
+    else if (this.generalItemStore.isInstanceOfGeneralItem(selectedProduct)) {
       this.store.selectedItem = this.generalItemStore.selectActualItem(selectedProduct);
-      this.navigateToCreateTransactions();
+    } 
+    else {
+      this.store.selectedItem = selectedProduct;
     }
   }
 
-  private navigateToCreateTransactions() {
+  navigateToCreateTransactions() {
     this.router.navigate(['../create-transactions'], {
       relativeTo: this.route,
     });
   }
 
-  get products$(): Observable<GeneralItem[] | Product[]> {
-    if ( this.store.kind === 'SALES' ) {
-      return this.generalItemStore.getAsObservable();
-    } 
-    return this.productService.getAsObservable();
+  private _filterProducts(value: string): GeneralItem[] | Product[] {
+    const filterValue = value.toLowerCase();
+    if (this.store.kind === 'SALES') {
+      return this.generalItemStore.getAsList().filter(
+        (item) => item.title.toLowerCase().includes(filterValue)
+      );
+    }
+    return this.productService.getAsList().filter(
+      (product) => product.title.toLowerCase().includes(filterValue)
+    );
   }
+
+  displayFn(product: GeneralItem | Product | string | undefined): string {
+    if (product === undefined || typeof(product) === 'string') {
+      return '';
+    }
+    return product && product.title ? product.title : '';
+  }
+  
 
   get hasTransactions(): Observable<boolean> {
     return this.store.invoice.pipe(
       map((value) => {
-        return value.transactions.length >= 1
+        return value.transactions.length >= 1;
       })
     );
   }
