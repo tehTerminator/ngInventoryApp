90e3229dbcd8f5917868988d39b59668421b94d4
Prateek Kher <prateek.kher@gmail.com>
2024-03-31 15:06:48 +0530

    invoice.component is never used

--- src/app/modules/secured/invoices/invoices.component.ts
+++ src/app/modules/secured/invoices/invoices.component.ts
@@ -1,21 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { InvoiceStoreService } from './services/invoice-store.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-invoices',
-  templateUrl: './invoices.component.html',
-  styleUrls: ['./invoices.component.css']
+  template: '',
 })
-export class InvoicesComponent implements OnInit {
-
-  constructor(
-    private store: InvoiceStoreService
-  ) { }
-
-  ngOnInit(): void {
-    // Initialize Services to Be Used Later
-    this.store.reset();
-  }
-
-
-}
+export class InvoicesComponent {}
