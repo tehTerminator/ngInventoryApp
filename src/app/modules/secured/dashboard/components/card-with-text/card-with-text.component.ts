881da07c383740f06bac2a593b793f01078799b2
Prateek Kher <prateek.kher@gmail.com>
2024-04-03 18:57:05 +0530

    Created Card With Text Component

---
+++ src/app/modules/secured/dashboard/components/card-with-text/card-with-text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-with-text',
+  templateUrl: './card-with-text.component.html',
+  styleUrl: './card-with-text.component.scss'
+})
+export class CardWithTextComponent {
+
+}
--- src/app/modules/secured/dashboard/dashboard.module.ts
+++ src/app/modules/secured/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { CardWithTextComponent } from './components/card-with-text/card-with-text.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, CardWithTextComponent],
   imports: [CommonModule, DashboardRoutingModule, MatTabsModule],
 })
 export class DashboardModule {}
