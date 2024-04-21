2ab0a815a8188b72849e7ad8740089442d9b2275
Prateek Kher <prateek.kher@gmail.com>
2024-04-21 23:43:09 +0530

    now User name visible in Menu

--- src/app/modules/secured/components/right-menu/right-menu.component.ts
+++ src/app/modules/secured/components/right-menu/right-menu.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { AuthenticationService } from '../../../../services/authentication/authentication.service';
+import { AuthStoreService } from '../../../../services/auth-store/auth-store.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-right-menu',
@@ -8,11 +10,18 @@ import { AuthenticationService } from '../../../../services/authentication/authe
 })
 export class RightMenuComponent {
   @Output() newClickEvent = new EventEmitter<any>();
-  constructor(private authService: AuthenticationService) {}
+  constructor(
+    private authService: AuthenticationService,
+    private authStore: AuthStoreService
+  ) {}
 
   onOpenCalc() {
     this.newClickEvent.emit(0);
   }
 
   logout = () => this.authService.signOut();
+
+  get title() {
+    return this.authStore.user.pipe(map((value) => value.name));
+  }
 }
