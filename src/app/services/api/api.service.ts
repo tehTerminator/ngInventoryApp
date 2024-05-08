53f3a0766a3649e453037cbe311a68becdf9abc5
Prateek Kher <prateek.kher@gmail.com>
2024-05-08 07:46:35 +0530

    using delay instead of debounceTime

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,46 +1,51 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.development';
-import { Observable, debounceTime, retry } from 'rxjs';
+import { Observable, debounceTime, delay, retry } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   private createUrl(prefix: string, urlData: string[] | string): string {
     let url = urlData;
     if (Array.isArray(urlData)) {
-      url = urlData.join('/')
+      url = urlData.join('/');
     }
     return `${environment.baseUrl}/${prefix}/${url}`;
   }
 
-  retrieve<T>(urlData: string | string[], payload?: {[key: string]: string}) {
+  retrieve<T>(urlData: string | string[], payload?: { [key: string]: string }) {
     const url = this.createUrl('get', urlData);
-    return this.http.get<T>(url, {params: payload}).pipe(debounceTime(this.randomTime()),retry(3))
+    return this.http
+      .get<T>(url, { params: payload })
+      .pipe(delay(this.randomTime()), retry(3));
   }
 
-
-  create<T>(url: string | string[], payload: {[key: string]: any}): Observable<T>{
-    const theUrl  = this.createUrl('create', url);
+  create<T>(
+    url: string | string[],
+    payload: { [key: string]: any }
+  ): Observable<T> {
+    const theUrl = this.createUrl('create', url);
     return this.http.post<T>(theUrl, payload);
   }
 
-  update<T>(url: string | string[], payload: {[key: string]: any}): Observable<T> {
-    const theUrl  = this.createUrl('update', url);
+  update<T>(
+    url: string | string[],
+    payload: { [key: string]: any }
+  ): Observable<T> {
+    const theUrl = this.createUrl('update', url);
     return this.http.put<T>(theUrl, payload);
   }
 
   delete<T>(url: string | string[], id: number): Observable<T> {
-    const theUrl  = this.createUrl('destroy', url);
+    const theUrl = this.createUrl('destroy', url);
     return this.http.delete<T>(`${theUrl}/${id}`);
   }
 
   private randomTime(): number {
-    return 100 + Math.floor(Math.random() * 100);
+    return 100 + Math.floor(Math.random() * 1000);
   }
-
 }
