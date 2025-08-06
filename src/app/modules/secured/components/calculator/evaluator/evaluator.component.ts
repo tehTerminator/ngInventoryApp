1ecb027c23f9f15154f5ac5b560d25e1918b7ba4
prateek.kher@gmail.com <prateek.kher@gmail.com>
2025-08-06 06:28:57 +0530

    moved to Components Folder

--- src/app/modules/secured/components/calculator/evaluator/evaluator.component.ts
+++ src/app/modules/secured/components/calculator/evaluator/evaluator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UntypedFormControl, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { evaluateString, mathPattern } from './../../../../shared/functions';
+import { evaluateString, mathPattern } from '../../../../../shared/functions';
 
 @Component({
     selector: 'app-evaluator',
